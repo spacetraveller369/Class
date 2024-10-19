df85ef923dfec892c435ba106456b67a7e8de146
Ілоночка <movlyanovai@gmail.com>
2024-10-19 21:15:36 +0300

    Добавьте файлы проекта.

---
+++ Class.cpp
@@ -0,0 +1,263 @@
+﻿#include <iostream>
+using namespace std;
+
+/* class Person {
+
+public:
+	string name;
+	int age;
+	string country;
+	double height;
+	double weight;
+	string color_hair;
+	string mood;
+	string hobby;
+	int energy;
+	string dream;
+	bool is_alive = true;
+
+	void Eat(string meal) {
+		cout << "Person is eating...\n";
+	}
+
+	void Sleep() {
+		cout << "Person is sleeping...\n";
+	}
+
+	void Walk(string street) {
+		cout << "Person is walking...\n";
+	}
+
+	void Work() {
+		cout << "Person is working...\n";
+	}
+
+	void Read() {
+		cout << "Person is reading...\n";
+	}
+
+	int main() {
+
+		int x;
+		Person person;
+		person.name = "Ilona";
+		person.age = 30;
+		person.country = "Ukraine";
+		person.height = 169;
+		person.weight = 65;
+		person.color_hair = "brown";
+		person.mood = "good";
+		person.hobby = "read";
+		person.energy = 20;
+		person.dream = "wealth";
+		person.Eat("food");
+		person.Sleep();
+		person.Work();
+		person.Read();
+	}
+	
+	class Cockroach {
+
+public:
+	string name;
+	double size;
+	int paws_count = 4;
+	string color;
+	string desire;
+	int energy = 100;
+	bool is_alive = true;
+	bool is_afwul = true;
+	bool is_dangerous = false;
+
+	void Eat(string meal) {
+		cout << "Cockroach is eating...\n";
+	}
+
+	void Sleep() {
+		cout << "Cockroach is sleeping...\n";
+	}
+
+	void Run(string slipper) {
+		cout << "Cockroach is running...\n";
+	}
+
+	void MakeDisaster() {
+		cout << "Cockroach is making a disaster everywhere he appears...\n";
+	}
+	
+	void Frighten() {
+		cout << "Cockroach frightens everyone in the house...\n";
+	}
+};
+
+int main() {
+
+	int x;
+	Cockroach cockroach; 
+	cockroach.color = "brown";
+	cockroach.energy = 100;
+	cockroach.size = 10;
+	cockroach.is_alive = true;
+	cockroach.desire = "eat";
+	cockroach.name = "Victor";
+	cockroach.paws_count = 4;
+	cockroach.Eat("garbage");
+	cockroach.Run("slipper");
+	cockroach.MakeDisaster();
+	cockroach.Frighten();
+
+}
+
+class Fridge {
+
+public:
+	string model;
+	int working_period;
+	string origin;
+	double height;
+	double weight;
+	string color;
+	string type;
+	int energy;
+	bool is_work = true;
+
+	void Freeze(string food) {
+		cout << "Fridge is freezing food good...\n";
+	}
+
+	void Give(string food) {
+		cout << "Fridge is giving me food...\n";
+	}
+
+	void Work() {
+		cout << "Fridge is working while electricity is in house...\n";
+	}
+
+	void Save() {
+		cout << "Fridge saves my masks and cremes...\n";
+	}
+
+	void Make() {
+		cout << "Fridge makes an ice...\n";
+	}
+	
+
+	int main() {
+
+		int x;
+		Fridge fridge;
+		fridge.model = "Liebherr";
+		fridge.working_period = 10;
+		fridge.origin = "Germany";
+		fridge.height = 150;
+		fridge.weight = 40;
+		fridge.color = "grey";
+		fridge.type = "domestic";
+		fridge.energy = 80;
+		fridge.is_work = true;
+		fridge.Freeze("food");
+		fridge.Give("food");
+		fridge.Save();
+		fridge.Work();
+		fridge.Make();
+
+
+	} 
+
+class Plant {
+
+public:
+	string kind;
+	int age;
+	string country;
+	double height;
+	double weight;
+	string color;
+	string condition;
+	int energy;
+	string desire;
+	bool is_alive = true;
+
+	void Drink(string water) {
+		cout << "Plant is drinking...\n";
+	}
+
+	void Grow() {
+		cout << "Plant is growing...\n";
+	}
+
+	void Give(string oxygen) {
+		cout << "Plant giving me an oxygen to breathe...\n";
+	}
+
+	void Joy() {
+		cout << "Plant gives a joy to my cat...\n";
+	}
+
+
+	int main() {
+
+		int x;
+		Plant plant;
+		plant.kind = "hibiscus";
+		plant.country = "Ukraine";
+		plant.height = 30;
+		plant.weight = 2;
+		plant.color = "green";
+		plant.condition = "good";
+		plant.energy = 90;
+		plant.desire = "water";
+		plant.Drink("water");
+		plant.Grow();
+		plant.Give("oxygen");
+		plant.Joy();
+	}
+	
+
+class Computer {
+
+public:
+	string kind;
+	int age;
+	string origin;
+	double height;
+	double weight;
+	string color;
+	string condition;
+	bool works = true;
+
+	void Eat(string electricity) {
+		cout << "Computer eats some electricity...\n";
+	}
+
+	void Teach() {
+		cout << "Computer teaches me...\n";
+	}
+
+	void Give(string money) {
+		cout << "Computer gives me some money...\n";
+	}
+
+	void Joy() {
+		cout << "Computer shows me films...\n";
+	}
+
+
+	int main() {
+
+		int x;
+		Computer computer;
+		computer.age = 2;
+		computer.origin = "China";
+		computer.height = 10;
+		computer.weight = 1;
+		computer.color = "black";
+		computer.condition = "good";
+		computer.Eat("electricity");
+		computer.Teach();
+		computer.Give("money");
+		computer.Joy();
+
+	}
+
+}; */
